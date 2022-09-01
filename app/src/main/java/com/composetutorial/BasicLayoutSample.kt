079dc53b75d35db1bc0bacdbdb9de3555fd4de5a
Sanjay <sanjaykumar8515@gmail.com>
2022-09-01 16:58:25 +0530

    Chapter3 completed with basic layout

---
+++ app/src/main/java/com/composetutorial/BasicLayoutSample.kt
@@ -0,0 +1,311 @@
+package com.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Spa
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.composetutorial.ui.theme.ComposeTutorialTheme
+import java.util.*
+
+class BasicLayoutSample : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+                    MySootheApp()
+        }
+    }
+}
+
+@Composable
+private fun MySootheApp(){
+    ComposeTutorialTheme{
+        Scaffold(
+            bottomBar = { SootheBottomNavigation()}
+        ) { padding ->
+            HomeScreen(Modifier.padding(padding))
+        }
+    }
+}
+
+@Composable
+private fun SootheBottomNavigation(
+    modifier: Modifier=Modifier
+){
+    BottomNavigation(
+        modifier=modifier,
+        backgroundColor = MaterialTheme.colors.background
+    ) {
+        BottomNavigationItem(
+            selected =true ,
+            icon = {
+                   Icon(imageVector = Icons.Default.Spa, contentDescription = null)
+            },
+            label = {
+                    Text(stringResource(R.string.bottom_navigation_home))
+            }
+            ,onClick = {}
+        )
+        BottomNavigationItem(
+            selected =false ,
+            icon = {
+                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = null)
+            },
+            label = {
+                Text(stringResource(R.string.bottom_navigation_profile))
+            }
+            ,onClick = {}
+        )
+
+    }
+}
+
+@Composable
+private fun HomeScreen(modifier: Modifier=Modifier){
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.height(16.dp))
+        SearchBar(Modifier.padding(horizontal = 16.dp))
+        HomeSection(title = R.string.align_your_body) {
+            AlignYourBodyRow()
+        }
+        HomeSection(title = R.string.favorite_collections) {
+            FavoriteCollectionsGrid()
+        }
+        Spacer(Modifier.height(16.dp))
+    }
+}
+
+@Composable
+private fun HomeSection(
+    @StringRes title:Int,
+    modifier: Modifier=Modifier,
+    content: @Composable ()-> Unit
+){
+    Column(modifier) {
+        Text(
+            text = stringResource(title).uppercase(Locale.getDefault()),
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier
+                .paddingFromBaseline(top = 40.dp, bottom = 8.dp)
+                .padding(horizontal = 16.dp)
+        )
+        content()
+    }
+}
+
+@Composable
+private fun SearchBar(
+    modifier: Modifier=Modifier
+){
+    TextField(value = "", onValueChange ={},
+        leadingIcon = {
+                      Icon(imageVector = Icons.Default.Search,
+                          contentDescription =null )
+        },
+        colors=TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder= {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp))
+}
+
+@Composable
+fun FavoriteCollectionsGrid(
+    modifier: Modifier=Modifier
+){
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding= PaddingValues( horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(120.dp)
+    ){
+        items(favoriteCollectionsData){ item ->
+            FavoriteCollectionCard(
+                drawable = item.drawable,
+                text = item.text,
+                modifier=Modifier.height(56.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyRow(
+    modifier: Modifier=Modifier
+){
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding= PaddingValues(horizontal = 16.dp),
+        modifier=modifier
+    ){
+        items(alignYourBodyData){item->
+            AlignYourBodyElement(item.drawable, item.text)
+        }
+    }
+}
+
+@Composable
+private fun AlignYourBodyElement(
+    @DrawableRes drawable:Int,
+    @StringRes text:Int,
+    modifier: Modifier=Modifier
+){
+    Column( horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(drawable) ,
+            contentDescription =null,
+            contentScale = ContentScale.Crop,
+             modifier = Modifier
+                 .size(88.dp)
+                 .clip(CircleShape))
+        Text(
+            text = stringResource(text),
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier.paddingFromBaseline(
+                top=24.dp, bottom = 8.dp
+            )
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun AlignYourBodyElementPreview() {
+    ComposeTutorialTheme {
+        AlignYourBodyElement(
+            text=R.string.inversions,
+            drawable = R.drawable.ab1_inversions,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Composable
+private fun FavoriteCollectionCard(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier=Modifier
+){
+    Surface(
+        shape = MaterialTheme.shapes.small,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.width(192.dp)
+        ){
+            Image(
+                painter = painterResource(drawable),
+                contentDescription =null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(56.dp)
+            )
+            Text(
+                text = stringResource(text),
+                style = MaterialTheme.typography.h6,
+                modifier=Modifier.padding( horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun FavoriteCollectionCardPreview() {
+    ComposeTutorialTheme {
+        FavoriteCollectionCard(
+            text=R.string.nature_meditations,
+            drawable = R.drawable.fc2_nature_meditations,
+            modifier = Modifier.padding(8.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2)
+@Composable
+fun HomeSectionPreview() {
+    ComposeTutorialTheme {
+       HomeSection(R.string.align_your_body) {
+           AlignYourBodyRow()
+       }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview3() {
+    ComposeTutorialTheme {
+        SearchBar()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF0EAE2, heightDp = 180)
+@Composable
+fun ScreenContentPreview() {
+    ComposeTutorialTheme {
+        HomeScreen()
+    }
+}
+
+
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
